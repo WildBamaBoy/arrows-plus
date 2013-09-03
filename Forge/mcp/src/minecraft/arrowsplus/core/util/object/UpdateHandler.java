66d130b2d5a67403107330285846bec76a0c649e
WildBamaBoy <dustya@otelco.net>
2013-09-02 21:17:55 -0500

    Up version number.

--- Forge/mcp/src/minecraft/arrowsplus/core/util/object/UpdateHandler.java
+++ Forge/mcp/src/minecraft/arrowsplus/core/util/object/UpdateHandler.java
@@ -26,7 +26,7 @@ import arrowsplus.core.util.Format;
 public class UpdateHandler implements Runnable
 {
 	/** The current version of Arrows Plus. */
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	
 	/** The update's compatible Minecraft version. */
 	public static String compatibleMinecraftVersion = "";
